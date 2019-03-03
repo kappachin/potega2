07ac2e172657f3bdb04f1c8244b855ab578a495c
kappachin <mino.aka.taru@gmail.com>
2019-03-03 20:28:49 +0100

    logika 3

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,10 +1,14 @@
 package App;
 import Logika.Logic;
+import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
         Logic logic = new Logic();
         System.out.println(logic.potegaWarunkowe(2,1));
-
+        long start = System.nanoTime();
+        System.out.println(logic.Potega(2,31));
+        long stop = System.nanoTime();
+        System.out.println((stop - start));
     }
 }
--- src/Logika/Logic.java
+++ src/Logika/Logic.java
@@ -21,8 +21,23 @@ public class Logic {
 
     public BigInteger Potega(long podstawa, int wykladnik){
         BigInteger wynik;
+
+
         if(wykladnik == 0){
             wynik = BigInteger.valueOf(1);
+        }else{
+            if ((wykladnik & 1) == 0){
+                //even
+                BigInteger temp = Potega(podstawa, (wykladnik / 2));
+                wynik = temp.multiply(temp);
+
+            } else {
+                //odd
+                BigInteger temp2 = Potega(podstawa, (wykladnik - 1));
+                wynik = temp2.multiply(BigInteger.valueOf(podstawa));
+            }
+
+
         }
 
        // ( a * pow(a,n-1))
