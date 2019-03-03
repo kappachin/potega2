ad93eadd66c630efb2e6ac66f8c5edb9f6600305
kappachin <mino.aka.taru@gmail.com>
2019-03-03 12:03:01 +0100

    logika 2

---
+++ src/App/Main.java
@@ -0,0 +1,10 @@
+package App;
+import Logika.Logic;
+public class Main {
+
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.potega(2,0));
+
+    }
+}
--- src/Logika/Logic.java
+++ src/Logika/Logic.java
@@ -1,16 +1,18 @@
+package Logika;
+
 import java.math.BigInteger;
 
 
-public class logic {
+public class Logic {
     public BigInteger potega(int podstawa, int wykladnik) {
         BigInteger wynik;
         if (wykladnik == 0) {
-            podstawa = 1;
+            wynik = BigInteger.valueOf(1);
         } else {
             if ((wykladnik & 1) == 0) {
-                wynik = BigInteger.valueOf((podstawa^(wykladnik/2))^2);
+                wynik = BigInteger.valueOf(0);
             } else {
-
+                wynik = BigInteger.valueOf(0);
             }
 
         }
